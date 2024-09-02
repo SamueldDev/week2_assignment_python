d8d9048fb9bd1cc58157637bd36a3001b03b8342
SamueldDev <youngsammy2018@gmail.com>
2024-09-02 10:08:25 +0100

    answers to week2 assignment

---
+++ week2_asignment.py
@@ -0,0 +1,20 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list2 = [50, 60, 70]
+
+my_list.extend(my_list2)
+
+my_list.pop()
+
+my_list.sort()
+
+print(my_list);
+
+index_of_30 = my_list.index(30)
+print(f"the index of 30 in my_list is : {index_of_30}")
